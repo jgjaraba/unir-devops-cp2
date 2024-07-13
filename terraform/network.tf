11925c3460d265e5ad6377b89d842e0a252640db
José GJ <josegjaraba@gmail.com>
2024-07-13 14:37:26 +0000

    updated network.tf

---
+++ terraform/network.tf
@@ -0,0 +1,35 @@
+resource "azurerm_virtual_network" "vn" {
+  name                = "jgjcp2_vn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "sn" {
+  name                 = "jgjcp2_sn"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "jgjcp2_nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "jgjcp2_ipc"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.10"
+    public_ip_address_id             = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "jgjcp2_pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
