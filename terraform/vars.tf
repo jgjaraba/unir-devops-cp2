dfd5b0c73ad1e64d45ebb4a9a41e0329f979ce3d
José GJ <josegjaraba@gmail.com>
2024-07-19 13:40:24 +0000

    added description to terraform/vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -18,6 +18,7 @@ variable "aks_vm_size" {
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  description = "Configuration file to connect to Kubernetes cluster"
   sensitive = true
 }
 
